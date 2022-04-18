2986e7005c59c95c36aaac097fc02a561ec91617
Marand <marpht23@gmail.com>
2022-04-18 09:34:58 +0200

    data and map list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const getData = () => {
+        fetch('data/positiv-liste-2022.json', {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (myJson) {
+                setPositivListe(myJson)
+            });
+    }
+    useEffect(() => {
+        getData()
+    })
+
+    let [positivListe, setPositivListe] = useState([])
+
+    return (
+        <div className="App">
+
+
+
+            {positivListe.map((item, i) =>
+                <div key={i}>
+                    <p>{item.Navn}</p>
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default App;
