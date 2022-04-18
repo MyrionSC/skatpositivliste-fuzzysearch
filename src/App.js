5ff9dd5b97fbc81d3ede17311a0f041a734d5477
Marand <marpht23@gmail.com>
2022-04-18 09:56:28 +0200

    create table

--- src/App.js
+++ src/App.js
@@ -26,13 +26,37 @@ function App() {
     return (
         <div className="App">
 
+            <table>
+                <thead>
+                <tr>
+                    <th>Registreringsland /Skattemæssigt hjemsted</th>
+                    <th>ISIN-kode</th>
+                    <th>Navn</th>
+                    <th>LEI kode</th>
+                    <th>ASIDENT</th>
+                    <th>CVR/SE/TIN</th>
+                    <th>Navn</th>
+                    <th>Første registreringsår</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                {positivListe.map((o, i) =>
+                    <tr key={i}>
+                        <td>{o.taxCountry}</td>
+                        <td>{o.ISIN_kode}</td>
+                        <td>{o.Navn}</td>
+                        <td>{o.LEI_kode}</td>
+                        <td>{o.ASIDENT}</td>
+                        <td>{o.CVR_SE_TIN}</td>
+                        <td>{o.Navn__1}</td>
+                        <td>{o.Første_registreringsår}</td>
+                    </tr>
+                )}
+                </tbody>
+            </table>
 
 
-            {positivListe.map((item, i) =>
-                <div key={i}>
-                    <p>{item.Navn}</p>
-                </div>
-            )}
         </div>
     );
 }
